914f9e531b318caa6c0c40617e2512d7e94ac8a1
SensorsIOT <andreas.spiess@arumba.com>
2018-09-13 21:32:52 +0200

    Error correction

--- LoRaSenderAtmel/LoRaSenderAtmel.ino
+++ LoRaSenderAtmel/LoRaSenderAtmel.ino
@@ -2,7 +2,7 @@
 #include <LoRa.h>
 
 // uncomment the section corresponding to your board
-// BSFrance 2017 contact@bsrance.fr 
+// BSFrance 2017 contact@bsrance.fr
 
 //  //LoR32u4 433MHz V1.0 (white board)
 //  #define SCK     15
@@ -11,7 +11,7 @@
 //  #define SS      1
 //  #define RST     4
 //  #define DI0     7
-//  #define BAND    433E6 
+//  #define BAND    433E6
 //  #define PABOOST true
 
 //  //LoR32u4 433MHz V1.2 (white board)
@@ -21,40 +21,39 @@
 //  #define SS      8
 //  #define RST     4
 //  #define DI0     7
-//  #define BAND    433E6 
-//  #define PABOOST true 
+//  #define BAND    433E6
+//  #define PABOOST true
 
-  //LoR32u4II 868MHz or 915MHz (black board)
-  #define SCK     15
-  #define MISO    14
-  #define MOSI    16
-  #define SS      8
-  #define RST     4
-  #define DI0     7
-  #define BAND    868E6  // 915E6
-  #define PABOOST true 
+//LoR32u4II 868MHz or 915MHz (black board)
+#define SCK     15
+#define MISO    14
+#define MOSI    16
+#define SS      8
+#define RST     4
+#define DI0     7
+#define BAND    868E6  // 915E6
+#define PABOOST true
 
-int counter = 0;
+long freq = 750E6;
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
   Serial.println("LoRa Sender");
-  LoRa.setPins(SS,RST,DI0);
-  if (!LoRa.begin(BAND,PABOOST)) {
+  LoRa.setPins(SS, RST, DI0);
+  if (!LoRa.begin(freq)) {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
+  LoRa.setTxPower(20, PABOOST);
+  LoRa.setSpreadingFactor(9);
 }
 
 void loop() {
-  Serial.print("Sending packet: ");
-  Serial.println(counter);
-  // send packet
-  LoRa.beginPacket();
-  LoRa.print("hello ");
-  LoRa.print(counter);
-  LoRa.endPacket();
-  counter++;
-  delay(5000);
+  for (freq = 750E6; freq < 999e6; freq = freq + 5e5) {
+    LoRa.setFrequency(freq);
+    LoRa.beginPacket();
+    LoRa.print("hello");
+    LoRa.endPacket();
+  }
 }
